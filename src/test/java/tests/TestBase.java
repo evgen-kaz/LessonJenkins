265a020ae5f3c9cde5c5e25aa04a9f730fe2ba6a
evgen-kaz <itroute@mail.ru>
2025-09-17 13:57:08 +0800

    added commit2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class TestBase {
 
     @BeforeEach
     void setupConfig() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
